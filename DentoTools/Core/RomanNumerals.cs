d9d03d11621fe631a79d6606286f92f93061e5e6
Dennise Catolos <63337204+dentolos19@users.noreply.github.com>
2021-08-13 17:16:26 +0800

    Added roman numerals converter

---
+++ DentoTools/Core/RomanNumerals.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DentoTools.Core
+{
+
+    public static class RomanNumerals
+    {
+
+        private static readonly Dictionary<char, int> RomanNumberDictionary = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        private static readonly Dictionary<int, string> NumberRomanDictionary = new()
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+        };
+
+        public static string ToRomanNumerals(int number)
+        {
+            var roman = new StringBuilder();
+            foreach (var item in NumberRomanDictionary)
+                while (number >= item.Key)
+                {
+                    roman.Append(item.Value);
+                    number -= item.Key;
+                }
+            return roman.ToString();
+        }
+
+        public static int FromRomanNumerals(string roman)
+        {
+            var total = 0;
+            var previousRoman = '\0';
+            foreach (var t in roman)
+            {
+                var previous = previousRoman != '\0' ? RomanNumberDictionary[previousRoman] : '\0';
+                var current = RomanNumberDictionary[t];
+                if (previous != 0 && current > previous)
+                    total = total - 2 * previous + current;
+                else
+                    total += current;
+                previousRoman = t;
+            }
+            return total;
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-<div align="center">
-
-<img src="./.github/icon.png" width="100"/>
-
-</div>
-
-<h1 align="center">DentoTools</h1>
-
-<div align="center">
+# <img src="./.github/icon.png" width="32"/> DentoTools
 
 [![](https://img.shields.io/badge/Powered%20By-.NET-blue?logo=microsoft&style=flat-square)](https://dotnet.microsoft.com)
 [![](https://img.shields.io/badge/Made%20With-Visual%20Studio-blue?logo=visual-studio&style=flat-square)](https://visualstudio.microsoft.com)
 
-</div>
+This project is built for my school, currently work-in-progress!
+
+## About This App
+
+This project was built to test people (particularily students) on their cyberwellness skills and how to protect them from fake news.
+
+### Features
+
+* Generators
+  * [X] Password Generator
+  * [X] GUID Generator
+  * [ ] QR Code Generator
+* Converters
+  * [X] Roman Numerals Converter
